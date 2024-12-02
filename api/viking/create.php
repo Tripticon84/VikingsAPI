e440d86a5bbdca7a3741005ace69ab3b7df5aaa7
Tripticon84 <yannjou@free.fr>
2024-12-02 12:10:11 +0100

    Mettre à jour la fonctionnalité Create du viking pour qu'il puisse être créé avec une arme par défaut si elle existe. Retourner une erreur appropriée si elle n'existe pas et ne pas créer le viking.

--- api/viking/create.php
+++ api/viking/create.php
@@ -3,6 +3,10 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/api/dao/viking.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/utils/server.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/viking/service.php';
 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/api/dao/weapon.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/api/weapon/findOne.php.php';
+
+
 header('Content-Type: application/json');
 
 if (!methodIsAllowed('create')) {
@@ -15,12 +19,16 @@ $data = getBody();
 if (validateMandatoryParams($data, ['name', 'weaponId', 'health', 'attack', 'defense'])) {
     verifyViking($data);
 
-    $newVikingId = createViking($data['name'], $data['weaponId'], $data['health'], $data['attack'], $data['defense']);
-    if (!$newVikingId) {
-        returnError(500, 'Could not create the viking');
+    if (findOneWeapon($data['weaponId'])) {
+        $newVikingId = createViking($data['name'], $data['weaponId'], $data['health'], $data['attack'], $data['defense']);
+        if (!$newVikingId) {
+            returnError(500, 'Could not create the viking');
+        }
+        echo json_encode(['id' => $newVikingId]);
+        http_response_code(201);
+    } else {
+        returnError(400, 'WeaponId not exist');
     }
-    echo json_encode(['id' => $newVikingId]);
-    http_response_code(201);
 } else {
     returnError(412, 'Mandatory parameters : name, health, attack, defense');
 }
